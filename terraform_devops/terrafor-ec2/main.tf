522a2cad4c930321bd6f0d9c7c4d4408b46cd64a
Dulanja Fernando <dulanjafernando524@gmail.com>
2026-02-16 09:09:34 +0530

    Configure EC2 instance with Docker and nginx
    Updated EC2 instance configuration to include Docker installation and run an nginx container. Added security group for SSH and HTTP access.

--- terraform_devops/terrafor-ec2/main.tf
+++ terraform_devops/terrafor-ec2/main.tf
@@ -8,19 +8,11 @@ terraform {
 }
 
 provider "aws" {
-  region     = "ap-south-1"
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-02eb7a4783e7e9317" # Amazon Linux 2 (ap-south-1)
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "Terraform-EC2-NoCLI"
-  }
-
-}
-  resource "aws_security_group" "web_sg" {
+# Security Group
+resource "aws_security_group" "web_sg" {
   name        = "terraform-web-sg"
   description = "Allow SSH and HTTP"
   vpc_id      = "vpc-0dc1d85425b512e49"
@@ -47,10 +39,28 @@ resource "aws_instance" "my_ec2" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "Terraform-Web-SG"
-  }
 }
 
+# EC2 Instance
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-02eb7a4783e7e9317"   # Amazon Linux 2 (ap-south-1)
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
+  tags = {
+    Name = "Terraform-EC2-Docker"
+  }
+
+  # Install Docker & Run Container Automatically
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              systemctl start docker
+              systemctl enable docker
+              usermod -aG docker ec2-user
+
+              # Run nginx container
+              docker run -d -p 80:80 nginx
+              EOF
+}
